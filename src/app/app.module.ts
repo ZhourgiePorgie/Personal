71d729a88d84f420400252ac400ff9d2d02ed5e5
Han Zhou <han_zhou@berkeley.edu>
2019-08-13 22:44:49 -0400

    added responsive scrollspy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { InViewportModule } from '@thisissoon/angular-inviewport';
+import { ScrollSpyModule } from '@thisissoon/angular-scrollspy';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,10 @@ import { MaterialModule } from './material/material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    InViewportModule,
+    ScrollSpyModule.forRoot(),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
 import { MatToolbarModule} from '@angular/material';
+import { MatTabsModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+
+
 
 const material = [
   MatButtonModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatTabsModule,
+  MatSidenavModule,
+  MatIconModule
 ]
 
 @NgModule({

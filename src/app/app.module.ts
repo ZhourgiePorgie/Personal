7b1da4a8c40263c47ee9f5475f4a93b635e68a79
Han Zhou <han_zhou@berkeley.edu>
2019-08-14 22:53:38 -0400

    updated vlogs page to be carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './navigation/nav-bar/nav-bar.component';
+import { SideNavComponent } from './navigation/side-nav/side-nav.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+
 import { InViewportModule } from '@thisissoon/angular-inviewport';
 import { ScrollSpyModule } from '@thisissoon/angular-scrollspy';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { SideNavComponent } from './navigation/side-nav/side-nav.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -25,7 +28,8 @@ import { SideNavComponent } from './navigation/side-nav/side-nav.component';
     MaterialModule,
     InViewportModule,
     ScrollSpyModule.forRoot(),
-    FlexLayoutModule
+    FlexLayoutModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
